48047462fd02f1d132425cf527b2727e252e872e
thiagocampanille <114732149+TCR7@users.noreply.github.com>
2023-08-01 23:34:45 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,83 @@
-from flask import Flask, render_template
+# Passo a passo
+# Passo 1: Entrar no sistema da "empresa"
+    # https://dlp.hashtagtreinamentos.com/python/intensivao/login
 
-from flask_socketio import SocketIO, send
+import pyautogui
+import time
+import pandas
+#pyautogui.write -> escrever um texto.
+#pyautogui.press -> apertar um tecla do teclado.
+#pyautogui.click -> click do mouse.
+#pyautogui.hotkey -> apertar duas teclas, e sepera por vírgula.
 
-app = Flask(__name__)
+pyautogui.PAUSE = 0.5
 
-socketio = SocketIO(app, cors_allowed_origins="*") # isso aqui é o tunel de comunicação entre os pcs
+# Abrir o navegador
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
 
-# funcionalidade de enviar mensagem
+# Entrar no link
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(3)
 
-@socketio.on("message")
-def gerenciar_mensagem(mensagem):
-    send(mensagem, broadcast=True)
 
-# Criar nossa primeira página = primeira rota
+# Passo 2: Fazer login
+# selecionar o campo de email
 
-@app.route("/")
-def homepage():
-    return render_template("index.html")
+pyautogui.click(x=785, y=368)
 
-# Rodar o app
-socketio.run(app, host="192.168.0.84")
+# escrever seu email
 
-# websocket -> tunel
+pyautogui.write("xesquedele22@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("senhasenhasenha")
+pyautogui.click(x=968, y=526)
+time.sleep(3)
+
+# Passo 3: Importar a base de produtos para cadastrar
+
+tabela = pandas.read_csv("produtos.csv")
+
+# Passo 4: Cadastrar um produto
+for linha in tabela.index: #index é o numero da linha da base de dados
+    # pegando a linha 0 da base de dados
+
+    # clicar no campo de código
+    pyautogui.click(x=840, y=245)
+    # pegar da tabela o valor do campo que a gente quer preencher
+
+    codigo = tabela.loc[linha, "codigo"]
+    # preencher o campo
+    pyautogui.write(str(codigo)) # lembre-se de sempre usar em string pra garantia de escrever um texto
+
+    # passar pro proximo
+
+    pyautogui.press("tab")
+    # preencher o campo
+    pyautogui.write(str(tabela.loc[linha, "marca"])) 
+    pyautogui.press("tab")
+
+    # preencher o campo
+    pyautogui.write(str(tabela.loc[linha, "tipo"])) 
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"])) 
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"])) 
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"])) 
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"])) 
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    # dar scroll pro topo da pagina
+
+    # pyautogui.scroll(5000) se necessario descer ou subir a pagina.
+
+    # Passo 5: Repetir o processo até o fim!
+
+
+    # Lembre-se sempre de fazer o passo a passo para saber o que fazer! Isso ajuda!
